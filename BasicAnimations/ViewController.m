0154c3f1a6729b04622a2722d6114285ecf3c44c
Mitchell Porter <mitchellporter@gmail.com>
2015-08-17 20:29:22 -0700

    pulsing opacity animation

--- BasicAnimations/ViewController.m
+++ BasicAnimations/ViewController.m
@@ -18,32 +18,31 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    UIView *myView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
-    myView.center = self.view.center;
-    myView.backgroundColor = [UIColor redColor];
-    [self.view addSubview:myView];
+    UIView *myView1 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    myView1.center = self.view.center;
+    myView1.backgroundColor = [UIColor redColor];
+    [self.view addSubview:myView1];
     
+    UIView *myView2 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    myView2.center = self.view.center;
+    myView2.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:myView2];
+    myView2.hidden = YES;
     
-    // Animation 1
-    CAKeyframeAnimation* widthAnim = [CAKeyframeAnimation animationWithKeyPath:@"borderWidth"];
-    NSArray* widthValues = [NSArray arrayWithObjects:@1.0, @10.0, @5.0, @30.0, @0.5, @15.0, @2.0, @50.0, @0.0, nil];
-    widthAnim.values = widthValues;
-    widthAnim.calculationMode = kCAAnimationPaced;
-    
-    // Animation 2
-    CAKeyframeAnimation* colorAnim = [CAKeyframeAnimation animationWithKeyPath:@"borderColor"];
-    NSArray* colorValues = [NSArray arrayWithObjects:(id)[UIColor greenColor].CGColor,
-                            (id)[UIColor redColor].CGColor, (id)[UIColor blueColor].CGColor,  nil];
-    colorAnim.values = colorValues;
-    colorAnim.calculationMode = kCAAnimationPaced;
-    
-    // Animation group
-    CAAnimationGroup* group = [CAAnimationGroup animation];
-    group.animations = [NSArray arrayWithObjects:colorAnim, widthAnim, nil];
-    group.duration = 20.0;
-    
-    [myView.layer addAnimation:group forKey:@"BorderChanges"];
-    
+    CABasicAnimation *anim = [CABasicAnimation animation];
+    anim.keyPath = @"opacity";
+    anim.repeatCount = 5;
+    anim.autoreverses = YES;
+    anim.fromValue = @(0.1);
+    anim.toValue = @(1);
+    anim.duration = 1;
+    [myView1.layer addAnimation:anim forKey:@"myAnim"];
+
+
+//    [UIView animateWithDuration:1.0 animations:^{
+//        //
+//        myView1.transform = CGAffineTransformMakeRotation(3.5);
+//    }];
 }
 
 - (void)didReceiveMemoryWarning {
